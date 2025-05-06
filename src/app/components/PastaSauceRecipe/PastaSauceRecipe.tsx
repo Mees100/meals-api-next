35320ab2827e910a33d8884d373e9493d9fabb10
Alina <alina100chuk@gmail.com>
2025-05-06 16:42:55 +0200

    feat: add component pasta Sause and async function

--- src/app/components/PastaSauceRecipe/PastaSauceRecipe.tsx
+++ src/app/components/PastaSauceRecipe/PastaSauceRecipe.tsx
@@ -1,3 +1,41 @@
-function PastaSausRecipe() {}
+"use client";
+import { useEffect, useState } from "react";
 
-export default PastaSausRecipe;
+type Meal = {
+  name: string;
+  ingredients: string[];
+  description: string;
+  slug: string;
+};
+
+type MealList = {
+  data: Meal[];
+  meta: object;
+};
+
+async function getData(): Promise<MealList | undefined> {
+  try {
+    const response = await fetch(
+      `https://meal-api-eight.vercel.app/meals?page=1&limit=10`
+    );
+
+    if (response.ok) {
+      return await response.json();
+    }
+    throw new Error("request failed");
+  } catch (error) {
+    console.error(error);
+  }
+}
+export default function PastaSauceRecipe() {
+  const [data, setData] = useState<Meal[]>([]);
+  useEffect(() => {
+    getData().then((r) => setData(r.data));
+  }, []);
+  const listItems = data.map((item) => <li key={item.slug}>{item.name}</li>);
+  return (
+    <>
+      <ul>{listItems}</ul>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PastaSauceRecipe from "./components/PastaSauceRecipe/PastaSauceRecipe";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <PastaSauceRecipe />
     </div>
   );
 }
