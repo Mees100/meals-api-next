792bb624a80ec7c52a70ca02bf0f8cf4385181c2
Alina <alina100chuk@gmail.com>
2025-05-15 16:27:40 +0200

    feat: async function POST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import PastaSauceRecipe from "./components/PastaSauceRecipe/PastaSauceRecipe";
+import FormulierMaaltijd from "./components/CreateMeal/CreatMeal";
 
 export default function Home() {
   return (
