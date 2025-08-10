94c3bd8cf6a273ea768f41209659cf8abfc98bc5
Alina <alina100chuk@gmail.com>
2025-08-10 17:57:12 +0200

    test: add login token test, and auto mock fetch

--- __tests__/Login.test.tsx
+++ __tests__/Login.test.tsx
@@ -1,12 +1,21 @@
-import Login from "@/app/components/Login/Login";
+import Login, { getToken } from "@/app/components/Login/Login";
 import "@testing-library/jest-dom";
-import {
-  fireEvent,
-  queryByTestId,
-  queryByText,
-  render,
-  screen,
-} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import fetchMock from "jest-fetch-mock";
+
+describe("Token", () => {
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  // See: https://www.browserstack.com/guide/jest-mock-fetch-requests
+  it("returns a token with valid credentials", async () => {
+    fetchMock.mockResponse(JSON.stringify({ access_token: "mocked data" }));
+    const actualToken = await getToken("me@example.com", "jhsdfjvhbdfxjvh");
+    expect(actualToken).toBeDefined();
+    expect(actualToken).toHaveProperty("access_token");
+  });
+});
 
 describe("Login", () => {
   it("renders button with text", () => {
